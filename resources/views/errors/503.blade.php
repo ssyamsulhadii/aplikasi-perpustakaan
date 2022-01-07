80feb69c55993fc558d0ed00d4cc2e0f9bccab24
c-syamsulhadi-si <c.syamsulhadi.si@gmail.com>
2022-01-07 16:10:12 +0800

    Menambahkan tampilan error

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,26 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>503 - Aplikasi Perpustakaan</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/error.css') }}">
+</head>
+
+<body>
+    <div id="error">
+        <div class="error-page container">
+            <div class="col-md-8 col-12 offset-md-2">
+                <img class="img-error" src="assets/images/samples/error-503.png" alt="Not Found">
+                <div class="text-center">
+                    <h1 class="error-title">Maintenance Mode</h1>
+                    <a href="/" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
